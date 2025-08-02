817c94aed13c23c6d63e09ada48368abc0515c01
chpill <etienne@spillemaeker.com>
2025-08-02 23:00:16 +0200

    Add an input for the last nixpkgs stable release

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
   description = "proxy flake to synchronise nixpkgs revision between multiple Nix flakes. See https://hugosum.com/blog/syncronizing-inputs-across-flakes";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-2505.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
   outputs = { self, nixpkgs }:{};
 }
